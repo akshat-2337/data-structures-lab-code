3477bc7c24e2d9a1ad094b980239e77a6d76b9a7
Akshat <109364428+akshat-2337@users.noreply.github.com>
2026-08-31 01:39:54 +0530

    Add selection sort implementation in week1_q3ii.c
    Implement selection sort algorithm to sort an array of integers.

---
+++ week1_q3ii.c
@@ -0,0 +1,30 @@
+/* Write a C program to sort a given list of elements using Selection Sort. */
+
+#include <stdio.h>
+
+int main() {
+    int n, a[100], i, j, min, temp;
+
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    for (i = 0; i < n - 1; i++) {
+        min = i;
+
+        for (j = i + 1; j < n; j++) {
+            if (a[j] < a[min])
+                min = j;
+        }
+
+        temp = a[i];
+        a[i] = a[min];
+        a[min] = temp;
+    }
+
+    for (i = 0; i < n; i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
